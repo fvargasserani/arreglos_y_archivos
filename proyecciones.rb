5452ff208144c54986d2708e5010a0c5749f356c
Francisca <fvargasserani@gmail.com>
2020-11-02 14:46:47 -0300

    added 3 more arguments

--- proyecciones.rb
+++ proyecciones.rb
@@ -1,25 +1,20 @@
-def read_file(filename)
-    original_data = open(filename).readlines
-    data = original_data.map { |x| Float(x) rescue nil }
-    n = data.size   
-    filtered_data = []
+def proyecciones(filename, ai, af, multiplicador)
+    original_data = open(filename).read.chomp.split(',')
+    data = original_data.map { |x| x.to_i }
+    new_data = []
+    n = data.count
     n.times do |i|
-        if data[i].is_a?(Float)
-            filtered_data << data[i]
+        if i >= ai && i <= af
+            new_data << (data[i] * multiplicador)
+        else
+            new_data << data[i]
         end
     end
+    total_sales = new_data.sum
+    total_sales_2float = []
+    total_sales_2float << '%0.2f'%total_sales
 end
 
-data = read_file('ventas_base.db')
-n = data.size
-sum_1 = 0
-sum_2 = 0
-n.times do |i|
-    if i <= 5
-        sum_1 += (data[i] * 1.1)
-    else
-        sum_2 += (data[i] * 1.2)
-    end
-end
+year_sales = proyecciones('ventas_base.db', 0, 5, 1.1) + proyecciones('ventas_base.db', 6, 11, 1.2)
 
-File.write('resultados.data',sum_1,sum_2)
+File.write('resultados.data', year_sales.join("\n"))
